f6f9211f9c79c361000e8fbb9f24ec63f96b3c07
gideonlangat98 <gideonlangat98@gmail.com>
2022-06-21 12:41:17 +0300

    Done.

--- main.js
+++ main.js
@@ -3,9 +3,28 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+function initialize () {
+  const likes = document.querySelector(".like-glyph");
+  console.log(likes)
 
+  for (const element of likes) {
+    element.addEventListener("click", (event) => {
+      mimicServerCall()
+      .then(()=> event.target.textContent = heartChange ["event.target.textContent"])
+  
+      .catch(error => {
+        const modal = document.getElementById("modal")
+        modal.classList.remove("hidden")
+        modal.querySelector("h2").textContent = error
+        setTimeout(function() {modal.classList.add("hidden")}, 3000)
 
+      })
+     }
+   )
+ }
+}
 
+initialize();
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
